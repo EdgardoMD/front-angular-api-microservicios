9c4a8eafab8748e34a7daf242813c3bfbdb46460
Edgardo Mora <edgardomora@hotmail.com>
2022-01-12 20:57:57 -0300

    agrega modulo layout + componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { ExamenesComponent } from './components/examenes/examenes.component';
+import { LayoutModule } from './layout/layout.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlumnosComponent,
     CursosComponent,
-    ExamenesComponent
+    ExamenesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
